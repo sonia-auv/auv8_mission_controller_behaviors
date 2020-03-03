eb2973d31233a8130177871dc650376b6a4ec2f3
francois <francois.cote-raiche.1@ens.etsmtl.ca>
2020-03-02 19:48:12 -0500

    relative y decouple

--- auv8_mission_controller_flexbe_states/src/auv8_mission_controller_flexbe_states/UnaprouvedState/moveRelativeYDecoupled.py
+++ auv8_mission_controller_flexbe_states/src/auv8_mission_controller_flexbe_states/UnaprouvedState/moveRelativeYDecoupled.py
@@ -7,15 +7,10 @@ from proc_control.srv import SetDecoupledTarget
 
 class MoveRelativeYDecoupled(EventState):
 
-    def __init__(self):
+    def __init__(self, distanceY):
         super(MoveRelativeYDecoupled, self).__init__(outcomes=['continue', 'failed'])
         self.target_reached = False
-
-    def define_parameters(self):
-        self.parameters.append(Parameter('param_distance_y', 1.0, 'Distance to travel'))
-
-    def get_outcomes(self):
-        return ['succeeded', 'aborted', 'preempted']
+        self.param_distance_y = distanceY
 
     def target_reach_cb(self, data):
         self.target_reached = data.target_is_reached
@@ -32,13 +27,14 @@ class MoveRelativeYDecoupled(EventState):
 
         except rospy.ServiceException as exc:
             rospy.loginfo('Service did not process request: ' + str(exc))
+            return 'failed'
 
         rospy.loginfo('Set relative decoupled position y = %f' % self.param_distance_y)
         self.target_reach_sub = rospy.Subscriber('/proc_control/target_reached', TargetReached, self.target_reach_cb)
 
     def execute(self, userdata):
         if self.target_reached > 0:
-            return 'succeeded'
+            return 'continue'
 
     def on_exit(self, userdata):
         self.target_reach_sub.unregister()
