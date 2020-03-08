3db57c602d6df437be2a260961f068b8738ad9ee
francois <francois.cote-raiche.1@ens.etsmtl.ca>
2020-03-07 21:11:10 -0500

    checksum

--- auv8_mission_controller_flexbe_behaviors/src/auv8_mission_controller_flexbe_behaviors/alignfrankalex_sm.py
+++ auv8_mission_controller_flexbe_behaviors/src/auv8_mission_controller_flexbe_behaviors/alignfrankalex_sm.py
@@ -63,7 +63,7 @@ class alignFrankAlexSM(Behavior):
 
 			# x:471 y:155
 			OperatableStateMachine.add('the best align',
-										AlignAlexFrank(object_height=450, object_width=300, distance_to_target=1, yaw_adjustment=10, max_time=100, topic_to_listen='/proc_image_processing/execution_953', image_height=1544, image_width=2064, speed=0.1, maximum_alignment=10, queue_size=10),
+										AlignAlexFrank(object_height=450, object_width=300, distance_to_target=1, yaw_adjustment=10, max_time=100, topic_to_listen='/proc_image_processing/execution_953_result', image_height=1544, image_width=2064, speed=0.1, maximum_alignment=10, queue_size=10),
 										transitions={'continue': 'finished', 'failed': 'failed'},
 										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off})
 
